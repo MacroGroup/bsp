0985311cc782f212c1dd2e3ae6d17d15029228ef
Alexander Shiyan <eagle.alexander923@gmail.com>
2023-07-20 18:57:18 +0300

    Add barebox to RK3568-MG-EVB build

--- boards/rk3568-mg-evb/build.sh
+++ boards/rk3568-mg-evb/build.sh
@@ -19,14 +19,15 @@ BASE=$(dirname -- $(readlink -f -- "$0"))
 ROOT=$BASE/../..
 OUTPUT=$ROOT/output/rk3568-mg-evb
 
-rm -fR $ROOT/output/download/linux
+#rm -fR $ROOT/output/download/linux
 
 cd $ROOT/modules
+git -C barebox pull origin macro 2&>/dev/null || git clone https://github.com/MacroGroup/barebox.git
 git -C buildroot pull origin macro 2&>/dev/null || git clone https://github.com/MacroGroup/buildroot.git
 git -C linux pull origin macro 2&>/dev/null || git clone https://github.com/MacroGroup/linux.git
 
 cd $ROOT/modules/buildroot
-git checkout macro
+git checkout origin/macro
 make defconfig BR2_DEFCONFIG=configs/rk3568_mg_evb_defconfig O=$OUTPUT
 make O=$OUTPUT
 
