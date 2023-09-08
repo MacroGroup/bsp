2f819c9e8f75dea860fcd9efee42291e3af5b28c
Alexander Shiyan <eagle.alexander923@gmail.com>
2023-09-08 08:05:35 +0300

    Add bsp version check

--- rk3568-mg-evb.sh
+++ rk3568-mg-evb.sh
@@ -13,13 +13,33 @@ for i in git make; do
 done
 
 ROOT=$(dirname -- $(readlink -f -- "$0"))
-OUTPUT=$ROOT/output/rk3568-mg-evb
-
 cd $ROOT
+
+git fetch || exit 1
+
+UPSTREAM="origin/master"
+LOCAL=$(git rev-parse @)
+REMOTE=$(git rev-parse $UPSTREAM)
+BASE=$(git merge-base @ $UPSTREAM)
+
+if [ $LOCAL != $REMOTE ]; then
+	if [ $LOCAL = $BASE ]; then
+		echo "The local BSP branch lags behind the remote one!"
+		echo "Do a 'git pull' first, then re-run $0."
+	elif [ $REMOTE = $BASE ]; then
+		echo "Local BSP branch overtakes remote! Probaly needs a push."
+	else
+		echo "Local and remote BSP branches are diverge!"
+	fi
+	exit 1
+fi
+
 git -C buildroot pull origin macro 2>/dev/null || git clone https://github.com/MacroGroup/buildroot.git
 
 cd $ROOT/buildroot
 git checkout macro || exit 1
+
+OUTPUT=$ROOT/output/rk3568-mg-evb
 make defconfig BR2_DEFCONFIG=configs/rk3568_mg_evb_defconfig O=$OUTPUT || exit 1
 
 cd $OUTPUT
