7096e65cf580e12e4bbf61c709f67df6b10c1716
Alexander Shiyan <eagle.alexander923@gmail.com>
2023-09-12 11:37:07 +0300

    Add build script for DS-IMX8M-EVB

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 This repository contains scripts for creating boot images used for debug boards that use the <a href="https://macrogroup.ru/">MacroGroup</a> <a href="https://diasom.ru/">modules</a>.
 
+To build a DS-IMX8M-EVB image get started with:
+```bash
+git clone https://github.com/MacroGroup/bsp.git
+cd bsp
+./ds-imx8m-evb.sh
+```
+
 To build a DS-RK3568-EVB image get started with:
 ```bash
 git clone https://github.com/MacroGroup/bsp.git
---
+++ ds-imx8m-evb.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+if [ $(id -u) == "0" ]; then
+	echo "This must be executed without root privilegies!"
+	exit 1
+fi
+
+for i in git make; do
+	if ! which $i >/dev/null 2>&1; then
+		echo "Program \"$i\" does not exists!"
+		exit 1
+	fi
+done
+
+ROOT=$(dirname -- $(readlink -f -- "$0"))
+
+cd $ROOT
+
+git fetch || exit 1
+
+UPSTREAM="origin/master"
+LOCAL=$(git rev-parse @)
+REMOTE=$(git rev-parse $UPSTREAM)
+BASE=$(git merge-base @ $UPSTREAM)
+
+if [ $LOCAL != $REMOTE ]; then
+	if [ $LOCAL = $BASE ]; then
+		echo "The local BSP branch lags behind the remote one!"
+		echo "Do a 'git pull' first, then re-run $0."
+	elif [ $REMOTE = $BASE ]; then
+		echo "Local BSP branch overtakes remote! Probaly needs a push."
+	else
+		echo "Local and remote BSP branches are diverge!"
+	fi
+	exit 1
+fi
+
+git -C buildroot pull origin macro 2>/dev/null || git clone https://github.com/MacroGroup/buildroot.git
+
+cd $ROOT/buildroot
+git checkout macro || exit 1
+
+OUTPUT=$ROOT/output/ds-imx8m-evb
+make defconfig BR2_DEFCONFIG=configs/diasom_imx8m_evb_defconfig O=$OUTPUT || exit 1
+
+cd $OUTPUT
+make
+
+exit 0
