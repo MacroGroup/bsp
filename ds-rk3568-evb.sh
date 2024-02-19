cf15dfe37ad81f4294875b8e763e1b0386070446
Alexander Shiyan <eagle.alexander923@gmail.com>
2024-02-19 15:12:43 +0300

    rk3568: Back to use macro branch

--- ds-rk3568-evb.sh
+++ ds-rk3568-evb.sh
@@ -34,12 +34,10 @@ if [ $LOCAL != $REMOTE ]; then
 	exit 1
 fi
 
-REV=396983ddd8
-
 git -C buildroot pull origin macro 2>/dev/null || git clone https://github.com/MacroGroup/buildroot.git
 
 cd $ROOT/buildroot
-git checkout $REV || exit 1
+git checkout macro || exit 1
 
 OUTPUT=$ROOT/output/ds-rk3568-evb
 make defconfig BR2_DEFCONFIG=configs/diasom_rk3568_evb_defconfig O=$OUTPUT || exit 1
